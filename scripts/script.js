10f072f96f00065ab2e9d5a7559f5dedb552274a
DanielBass <daniel.h.furtado.neto2@gmail.com>
2025-06-10 14:09:25 -0300

    ADD XLSX.

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,35 @@
 const URL_CANDIDATOS = "https://srdiadev-n8n-webhook.8qlb9b.easypanel.host/webhook/36835167-44f6-45fb-b72d-71f9e3694af8";
 const URL_REQUISITOS = "https://srdiadev-n8n-editor.8qlb9b.easypanel.host/webhook-test/formulario";
 
-// Envio do CSV de candidatos
-document.getElementById("formCandidatos").addEventListener("submit", async function (e) {
+// Tipos permitidos
+const allowedTypes = [
+  "text/csv",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+];
+
+// Envio do arquivo de candidatos
+document.getElementById("formCandidatos").addEventListener("submit", async e => {
   e.preventDefault();
   const file = document.getElementById("fileCandidatos").files[0];
-  if (!file) return alert("Selecione um arquivo CSV.");
+  if (!file) {
+    return alert("Selecione um arquivo CSV ou XLSX.");
+  }
+  if (!allowedTypes.includes(file.type)) {
+    return alert("Tipo de arquivo inválido. Use CSV ou XLSX.");
+  }
   await enviarArquivo(file, URL_CANDIDATOS);
 });
 
-// Envio do CSV de requisitos da vaga
-document.getElementById("formRequisitos").addEventListener("submit", async function (e) {
+// Envio do arquivo de requisitos
+document.getElementById("formRequisitos").addEventListener("submit", async e => {
   e.preventDefault();
   const file = document.getElementById("fileRequisitos").files[0];
-  if (!file) return alert("Selecione um arquivo CSV.");
+  if (!file) {
+    return alert("Selecione um arquivo CSV ou XLSX.");
+  }
+  if (!allowedTypes.includes(file.type)) {
+    return alert("Tipo de arquivo inválido. Use CSV ou XLSX.");
+  }
   await enviarArquivo(file, URL_REQUISITOS);
 });
 
